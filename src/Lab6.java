8d5872d851eb3f05b671e280fd334f739247b563
CarloQuick <98628846+CarloQuick@users.noreply.github.com>
2022-03-10 21:18:50 -0500

    Lab6 now subtracting

--- src/Lab6.java
+++ src/Lab6.java
@@ -4,19 +4,15 @@ public class Lab6 {
     public static void main(String[] args) {
         Scanner scnr = new Scanner(System.in);
 
-        System.out.println("Adding two numbers");
+        System.out.println("Subtracting two numbers (first value - second value)");
         System.out.print("\nEnter your 1st Value! ");
         int val1 = scnr.nextInt();
 
         System.out.print("\nEnter your 2nd Value! ");
         int val2 = scnr.nextInt();
 
-        int total = val1 + val2;
+        int difference = val1 - val2;
 
-        System.out.println(val1 + " + " + val2 + " = " + total);
-
-
-
-
+        System.out.println(val1 + " - " + val2 + " = " + difference);
     }
 }
